4d78e32b6abd941ab605d772b29cec7dbb3e933e
Fellipe Lima <140414505+francafellipe@users.noreply.github.com>
2024-05-10 12:28:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+/* Função para calcular a média que deve funcionar com qualquer número de alunos ou turma */
+/*let notas = [];
+
+function adicionarNota() {
+  let nota = parseFloat(prompt("Insira uma nota:"));
+  notas.push(nota);
+}
+
+function calcularMedia(notas) {
+  let soma = 0;
+  for (let nota of notas) {
+    soma += nota;
+  }
+  return soma / notas.length;
+}
+
+let escolha = parseInt(prompt("Escolha uma opção:\n1 - Adicionar uma nota\n2 - Visualizar média\n0 - Finalizar"));
+
+if (escolha === 1) {
+  adicionarNota();
+} else if (escolha === 2) {
+  let mediaTurma = calcularMedia(notas);
+  console.log(`A média da turma é: ${mediaTurma.toFixed(2)}`);
+} else {
+  console.log("Programa finalizado.");
+}
+*/
+let array = "chunks"
+let resultado 
+
+function multiplos(array){
+  resultado = (array + "-")* 10
+  
+}
+
+console.log(resultado)
